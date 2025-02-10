503ab260f4ba385434414e0c491d23797bc26409
narobe <narobekenea@gmail.com>
2025-02-10 17:03:23 +0300

    working on nave theming

---
+++ dist/js/theme.js
@@ -0,0 +1,22 @@
+const themeToggle = document.querySelector(".--themetoggle--");
+// localStorage.theme = "dark_theme";
+localStorage.theme == "dark_theme"
+  ? (document.documentElement.classList = "dark")
+  : (document.documentElement.classList = "light");
+
+themeToggle.addEventListener("click", () => {
+  // document.querySelectorAll("*:not(.no-theme-transition)").forEach((el) => {
+  //   el.style.transition = "1800ms";
+  // });
+  // setTimeout(() => {
+  //   document.querySelectorAll("*:not(.no-theme-transition)").forEach((el) => {
+  //     el.style.transition = "all 0ms";
+  //   });
+  // }, 2000);
+  localStorage.theme == "dark_theme"
+    ? (localStorage.theme = "light_theme")
+    : (localStorage.theme = "dark_theme");
+  localStorage.theme == "dark_theme"
+    ? (document.documentElement.classList = "dark")
+    : (document.documentElement.classList = "light");
+});

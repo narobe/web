b9e275c635486ec97fcf9f8e6e61e9f7d2f08321
narobe <narobekenea@gmail.com>
2025-02-10 19:05:16 +0300

    done with mobile nav

--- dist/js/nav.js
+++ dist/js/nav.js
@@ -1,25 +1,42 @@
-const navItem = document.querySelectorAll(".--navitem--");
-const navItemActive = document.querySelector(".--navitem--.on");
-const nav = document.querySelector(".--nav--");
+const navItem = document.querySelectorAll(".--navitem--:not(.--mobile--)");
+const mobileNavItem = document.querySelectorAll(".--navitem--.--mobile--");
+const navItemActive = document.querySelector(
+  ".--navitem--:not(.--mobile--).on",
+);
+const mobileNavItemActive = document.querySelector(
+  ".--navitem--.--mobile--.on",
+);
+const nav = document.querySelector(".--nav--:not(.--mobile--)");
+const mobileNav = document.querySelector(".--nav--.--mobile--");
 
 localStorage.cl = "false";
 
-function navEvent(el) {
+function navEvent(el, mob = false) {
   if (!el) return;
-  const target = el;
-  navItem.forEach((n) => {
-    n.classList.remove("on");
-  });
-  target.classList.add("on");
-}
-navEvent(navItemActive ?? null);
-
-nav.addEventListener("click", (e) => {
   localStorage.cl = "true";
   setTimeout(() => {
     localStorage.cl = "false";
   }, 600);
+  if (mob) {
+    mobileNavItem.forEach((n) => {
+      n.classList.remove("on");
+    });
+    el.classList.add("on");
+    return;
+  }
+  navItem.forEach((n) => {
+    n.classList.remove("on");
+  });
+  el.classList.add("on");
+}
+navEvent(mobileNavItemActive, true ?? null);
+navEvent(navItemActive ?? null);
+
+nav.addEventListener("click", (e) => {
   navEvent(e.target.closest(".--navitem--"));
+});
+mobileNav.addEventListener("click", (e) => {
+  navEvent(e.target.closest(".--navitem--"), true);
 });
 
 // navItem.forEach((item) => {

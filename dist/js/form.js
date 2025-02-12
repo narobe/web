2ca3a01441bbe76995fc4a198a32a245ee3c28d6
narobe <narobekenea@gmail.com>
2025-02-12 16:10:32 +0300

    add ptoject description

---
+++ dist/js/form.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("form");
+const formName = document.querySelector(".--form-name--");
+const formAdd = document.querySelector(".--form-address--");
+const formMessage = document.querySelector(".--form-message--");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(formName.value, formAdd.value, formMessage.value);
+  sendMessage();
+});
+
+async function sendMessage() {
+  const url = `https://api.telegram.org/bot7683880675:AAEU8jWuT_Zjo6EVCuMGTQj2uzgpnmwAa84/sendMessage?chat_id=-4743606472&text=<b>Name:</b>%20${formName.value}%0A<b>Phone:</b>%20${formAdd.value}%0A<b>Message:</b>%20${formMessage.value}&parse_mode=HTML`;
+  const response = await fetch(url);
+  const data = await response.json();
+  if (response.ok) {
+    form.reset();
+    alert("form sent!");
+  } else {
+    alert("form not sent!");
+  }
+}
